e567a2610138cfca5a114f8f68721728fab757d4
Aditya Priyadarshi <54215788+adityapriyadarshi669@users.noreply.github.com>
2020-05-22 12:35:46 +0530

    Added the files

---
+++ PyShop/products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+# Create your views here.
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products':products})
+
+
+def new(request):
+    return HttpResponse('New Products')
