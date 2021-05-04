f4b0241c6635f9a686897f6b155eb2d648e92d46
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-05-04 14:40:45 +0530

    Done some more thingys

--- PyShop/products/models.py
+++ PyShop/products/models.py
@@ -7,8 +7,14 @@ class Product(models.Model):
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
 
+    def __str__(self):
+        return self.name
+
 
 class Offer(models.Model):
     code = models.CharField(max_length=10)
     decription = models.CharField(max_length=256)
     discount = models.FloatField()
+
+    def __str__(self):
+        return self.code
--- PyShop/products/urls.py
+++ PyShop/products/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-
     path('', views.index),
     path('new', views.new)
 ]
